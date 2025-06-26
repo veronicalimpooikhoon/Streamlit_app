01b784d4f05a69be066f8656885b1ccec7ab3f2e
veronicalimpooikhoon <55375541+veronicalimpooikhoon@users.noreply.github.com>
2025-06-26 16:42:30 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -3,11 +3,7 @@ import pandas as pd
 import numpy as np
 import pickle
 import base64
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+
 
 @st.cache_data
 def get_fvalue(val):
